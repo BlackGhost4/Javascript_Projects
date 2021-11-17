09b8e01a3f9235d8c799694e97cff7c6ad332f39
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-17 15:38:09 -0800

    Update all files And added the portfolio

---
+++ JS_Portfolio_Student/js/portfolio.js
@@ -0,0 +1,61 @@
+// These functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+setInterval(function(){plusSlides(1)}, 4000);
+//showSlides(slideIndex);
+
+// This function changes the slide when the left or right arrows are clicked
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// This function changes the slide when the dots are clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
+    var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
+    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
+    }
+    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
+    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+}
+
+// SECTION FOR MODAL GAME
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+function openModalGame() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+        modal.style.display = "none";
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Javascript_Projects
+# HTML-and-CSS-Projects
 This repository contains all exercises of Javascript Course.
+
+## Projects
+
+* **Basic JavaScript Projects**
+* **Calculator**
+* **JavaScript Projects Desktop**
+* **JS Portfolio Student**
+* **Pizza Project**
+* **Portfolio Website**
+* **React Projects**
+* **Tic Tac Toe Game**
+
+### Basic JavaScript Projects
+
+Contains basic JavaScript exercises.
+
+### Calculator
+
+Contains the Calculator exercise.
+
+### JavaScript Projects Desktop
+
+Contains various exercises done with JavaScript.
+
+### JS Portfolio Student
+
+Has the Portfolio updated with JavaScript.
+
+### Pizza Project
+
+He has the project of a pizzeria menu.
+
+### Portfolio Website
+
+It has the portfolio website.
+
+### React Projects
+
+Contains exercises done with React.
+
+### Tic Tac Toe Game
+
+It has the Tic Tac Toe Game.
